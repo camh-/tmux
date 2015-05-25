16e8f843e5670184490a4e536e34e34897acfbc9
Cam Hutchison <camh@xdna.net>
2015-05-25 16:38:25 +1000

    show-environment: Add posix output mode.
    Add a -p option to show-environment to output the environment variables
    in a form that can be eval'ed by a posix-compatible shell such that the
    shell environment will reflect the session/global environment.
    
    Set variables are output as 'VAR="VAL"; export VAR;' (with appropriate
    escaping for special characters inside the double quotes), and unset
    variables are output as 'unset VAL;'.
    
    This makes is simple to propagate the session environment to an existing
    shell in a tmux window by running 'eval $(tmux show-environment -p)'.

--- cmd-show-environment.c
+++ cmd-show-environment.c
@@ -27,12 +27,15 @@
  * Show environment.
  */
 
-enum cmd_retval	 cmd_show_environment_exec(struct cmd *, struct cmd_q *);
+enum cmd_retval  cmd_show_environment_exec(struct cmd *, struct cmd_q *);
+
+char	*posix_dquote_escape(const char *value);
+void	 print_envent(struct cmd_q *, struct environ_entry *, int pflag);
 
 const struct cmd_entry cmd_show_environment_entry = {
 	"show-environment", "showenv",
-	"gt:", 0, 1,
-	"[-g] " CMD_TARGET_SESSION_USAGE " [name]",
+	"gpt:", 0, 1,
+	"[-gp] " CMD_TARGET_SESSION_USAGE " [name]",
 	0,
 	cmd_show_environment_exec
 };
@@ -59,19 +62,51 @@ cmd_show_environment_exec(struct cmd *self, struct cmd_q *cmdq)
 			cmdq_error(cmdq, "unknown variable: %s", args->argv[0]);
 			return (CMD_RETURN_ERROR);
 		}
-		if (envent->value != NULL)
-			cmdq_print(cmdq, "%s=%s", envent->name, envent->value);
-		else
-			cmdq_print(cmdq, "-%s", envent->name);
+		print_envent(cmdq, envent, args_has(self->args, 'p'));
 		return (CMD_RETURN_NORMAL);
 	}
 
 	RB_FOREACH(envent, environ, env) {
+		print_envent(cmdq, envent, args_has(self->args, 'p'));
+	}
+
+	return (CMD_RETURN_NORMAL);
+}
+
+char *
+posix_dquote_escape(const char *value)
+{
+	/* at most, we double the string length, escaping each char */
+	char	 c, *ret;
+	char	*out = ret = xmalloc(strlen(value)*2 + 1);
+
+	while ((c = *value++)) {
+		/* posix interprets $ ` " and \ in double quotes. */
+		if (c == '$' || c == '`' || c == '"' || c == '\\')
+			*out++ = '\\';
+		*out++ = c;
+	}
+	*out = '\0';
+
+	return ret;
+}
+
+void
+print_envent(struct cmd_q *cmdq, struct environ_entry *envent, int pflag)
+{
+	if (pflag) {
+		if (envent->value != NULL) {
+			char *escaped = posix_dquote_escape(envent->value);
+			cmdq_print(cmdq, "%s=\"%s\"; export %s;", envent->name,
+				escaped, envent->name);
+			free(escaped);
+		} else {
+			cmdq_print(cmdq, "unset %s;", envent->name);
+		}
+	} else {
 		if (envent->value != NULL)
 			cmdq_print(cmdq, "%s=%s", envent->name, envent->value);
 		else
 			cmdq_print(cmdq, "-%s", envent->name);
 	}
-
-	return (CMD_RETURN_NORMAL);
 }
